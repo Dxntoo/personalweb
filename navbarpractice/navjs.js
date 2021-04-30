43c3ee18f70aac485ed7d5622f5787d7952aa768
Dxntoo <44705303+Dxntoo@users.noreply.github.com>
2021-04-30 17:31:56 +0800

    added new files

---
+++ navbarpractice/navjs.js
@@ -0,0 +1,11 @@
+
+//function to enable the hamburger dropdown
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
---
+++ navbarpractice/navpractice2/navjs.js
@@ -0,0 +1,9 @@
+
+function displayMenuHB(){
+    var menulinks = document.getElementById("navbar__items");
+    if(menulinks.style.display == "block"){
+        menulinks.style.display == "none";
+    }else{
+        menulinks.style.display == "block";
+    }
+}
