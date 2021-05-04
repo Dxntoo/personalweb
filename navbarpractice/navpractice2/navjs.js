d8ae962da3319b3633a429d3011be8e9d3bb5523
Dxntoo <44705303+Dxntoo@users.noreply.github.com>
2021-05-05 03:23:58 +0800

    formatted using Prettier

--- navbarpractice/navpractice2/navjs.js
+++ navbarpractice/navpractice2/navjs.js
@@ -1,43 +1,32 @@
-   
-
 var menulinks = document.getElementById("navbar__items");
 var dropdown = document.getElementById("dropdown__menu");
 
- function myFunction(x) {
-    if (x.matches) {
-      menulinks.style.display = "none";
-      
-    }else{
-      menulinks.style.display = "inherit";
-    }
-
-    
-}
-  
-   var x = window.matchMedia("(max-width: 800px)");
-   myFunction(x) // Call listener function at run time
-   x.addListener(myFunction) // Attach listener function on state changes
-
-
-function hamburgerMenu(){
-    
-    if(dropdown.style.display == "none"){
-        dropdown.style.display = "flex"; 
-    }else{
-        dropdown.style.display = "none"; 
-    }
+function myFunction(x) {
+  if (x.matches) {
+    menulinks.style.display = "none";
+  } else {
+    menulinks.style.display = "inherit";
+  }
 }
 
+var x = window.matchMedia("(max-width: 800px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
+
+function hamburgerMenu() {
+  if (dropdown.style.display == "none") {
+    dropdown.style.display = "flex";
+  } else {
+    dropdown.style.display = "none";
+  }
+}
 
 function myFunction2(y) {
-    if (y.matches) {
-        dropdown.style.display = "none"; 
-      
-    }
-
-    
+  if (y.matches) {
+    dropdown.style.display = "none";
+  }
 }
-  
-   var y = window.matchMedia("(min-width: 800px)");
-   myFunction2(y) // Call listener function at run time
-   y.addListener(myFunction2) // Attach listener function on state changes
+
+var y = window.matchMedia("(min-width: 800px)");
+myFunction2(y); // Call listener function at run time
+y.addListener(myFunction2); // Attach listener function on state changes
